9cc54a86c21d7a5544a243e66db5a44c4e032940
Tamojit Das <tamojitdas181007@gmail.com>
2024-12-23 21:57:01 +0530

    UI updates & elevated user middleware

--- backend/routes/elevatedUser.routes.js
+++ backend/routes/elevatedUser.routes.js
@@ -1,13 +1,14 @@
 import express from "express";
 import { createImage, createMetaData, getImageFromBuckets, updateMetadata, updatePrediction } from "../controllers/elevatedUser.controller.js";
 import verifyToken from "../middleware/protectRoute.js";
+import verifyUser from "../middleware/elevatedUserProtected.js";
 
 const router = express.Router();
 
-router.patch("/update/:id", verifyToken, updatePrediction);
+router.patch("/update/:id", verifyToken, verifyUser, updatePrediction);
 router.post("/metadata", verifyToken, createMetaData);
-router.get("/get-images", verifyToken, getImageFromBuckets);
-router.patch("/update-metadata/:id", verifyToken, updateMetadata);
-router.post("/create-image", verifyToken, createImage);
+router.get("/get-images", verifyToken, verifyUser,  getImageFromBuckets);
+router.patch("/update-metadata/:id", verifyToken, verifyUser, updateMetadata);
+router.post("/create-image", verifyToken, verifyUser, createImage);
 
 export default router;
--- frontend/src/components/OrderCard.jsx
+++ frontend/src/components/OrderCard.jsx
@@ -1,23 +1,22 @@
-/* eslint-disable react/prop-types */
 const OrderCard = ({order}) => {
 	return (
 		<div className="p-2 bg-gray-300 rounded-lg flex gap-2">
 			<img src={order.image_url} alt={order.product_id} className="h-[150px] w-[160px] rounded-md" />
 
 			<div className="flex justify-between w-full">
-				<div className="flex flex-col max-w-[150px]">
+				<div className="flex flex-col max-w-[150px] lg:max-w-[300px]">
 					<h1 className="font-bold text-xl mb-1">Product Details</h1>
 					<span className="font-semibold text-xl">{order.product_name}</span>
 					<span className="text-gray-700">{order.seller_name}</span>
 					<span className="text-sm text-gray-600">{order.seller_type}</span>
-					<span className="text-sm text-gray-500">id: {order.product_id}</span>
+					<span className="text-sm text-gray-500 break-words">id: {order.product_id}</span>
 				</div>
 
-				<div className="flex flex-col max-w-[200px] mr-2">
-					<h1 className="font-bold text-xl mb-1">Customer Details</h1>
-					<span className="font-normal text-xl">{order.customer_name}</span>
-					<span className="text-gray-700 whitespace-normal">{order.customer_email}</span>
-					<span className="text-sm text-gray-500">{order.customer_mobile}</span>
+				<div className="flex flex-col max-w-[200px] lg:max-w-[300px] mr-2">
+					<h1 className="font-bold text-xl mb-1 break-words">Customer Details</h1>
+					<span className="font-normal text-xl break-words">{order.customer_name}</span>
+					<span className="text-gray-700 whitespace-normal break-words">{order.customer_email}</span>
+					<span className="text-sm text-gray-500 break-words">{order.customer_mobile}</span>
 				</div>
 
 				<div className="flex flex-col max-w-[200px]">
